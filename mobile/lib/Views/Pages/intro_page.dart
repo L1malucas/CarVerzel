b758b81fd011ad7ef20ef14002dbe8ac4c5c30a6
Lucas Lima <lucaslima.94@hotmail.com.br>
2023-07-20 18:26:42 -0300

    intro page ok

---
+++ mobile/lib/Views/Pages/intro_page.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:mobile/Views/Pages/car_list_page.dart';
+import 'package:mobile/Views/Widgets/fixed_spacer.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import '../../utils/constants.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade400.withOpacity(0.5),
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: Center(
+          child: Column(
+            // crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'CarVerzel',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 32),
+              ),
+              FixedSpacer.vBiggest(),
+              SvgPicture.asset('assets/images/logo_verzel.svg',
+                  height: 150,
+                  width: double.infinity,
+                  semanticsLabel: 'Verzel Logo'),
+              FixedSpacer.vBigger(),
+              const Text(
+                'Vitrine de Carros',
+                style: TextStyle(color: Colors.white, fontSize: 26),
+              ),
+              FixedSpacer.vBigger(),
+              const Text(
+                'Teste técnico, feito por Lucas Lima',
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              FixedSpacer.vSmaller(),
+              InkWell(
+                child: Text(
+                  style: TextStyle(color: Colors.blue.shade100, fontSize: 20),
+                  'Repositório Github',
+                ),
+                onTap: () {
+                  launchUrl(Constants.urlGithubRepo,
+                      mode: LaunchMode.externalApplication);
+                },
+              ),
+            ],
+          ),
+        ),
+      )),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(28.0),
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(minHeight: 60),
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              padding: const EdgeInsets.all(24.0),
+            ),
+            child: const Text(
+              'Vamos Começar!',
+              style: TextStyle(color: Colors.black87, fontSize: 20),
+            ),
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const CarListPage()));
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/Views/Pages/intro_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,14 +10,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      home: const IntroPage(),
       theme: ThemeData(useMaterial3: true),
-      debugShowCheckedModeBanner: false, 
+      debugShowCheckedModeBanner: false,
     );
   }
 }
