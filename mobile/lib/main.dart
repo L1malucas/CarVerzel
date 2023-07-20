8e0d6f2e997d05d33918ac9cefa5a2e5cc8e1ee2
lucas lima <lucaslj@ufba.br>
2023-07-20 09:16:42 -0300

    add mobile structure

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -9,12 +9,14 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       home: Scaffold(
         body: Center(
           child: Text('Hello World!'),
         ),
       ),
+      theme: ThemeData(useMaterial3: true),
+      debugShowCheckedModeBanner: false, 
     );
   }
 }
