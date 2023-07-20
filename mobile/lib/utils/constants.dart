6ccb7a4e1849fcce4eee24983275c5664659251e
Lucas Lima <lucaslima.94@hotmail.com.br>
2023-07-20 19:36:13 -0300

    add api url and fix url repo

--- mobile/lib/utils/constants.dart
+++ mobile/lib/utils/constants.dart
@@ -2,5 +2,7 @@ class Constants {
   static final Uri urlLinkedinProfile =
       Uri.parse('https://www.linkedin.com/in/limalucasdev/');
   static final Uri urlGithubRepo =
-      Uri.parse('https://github.com/L1malucas/JwtApi');
+      Uri.parse('https://github.com/L1malucas/CarVerzel');
+  static final Uri apiAzure =
+      Uri.parse('https://carverzelapi.azurewebsites.net/api');
 }
