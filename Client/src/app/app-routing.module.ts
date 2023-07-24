33ecd56c46fba20c05ff5ab4fe9e6c1c7245aec6
Lucas Lima <lucaslima.94@hotmail.com.br>
2023-07-24 10:17:26 -0300

    fix edit route show separeted

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './Components/login/login.component';
 import { CarFormComponent } from './Components/car-form/car-form.component';
 import { CarEditComponent } from './Components/car-edit/car-edit.component';
+import { CarListComponent } from './Components/car-list/car-list.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'car-list', pathMatch: 'full' },
+  { path: '', component: CarListComponent },
   { path: 'login', component: LoginComponent },
   { path: 'add-car', component: CarFormComponent },
   { path: 'carros/:id', component: CarEditComponent },
